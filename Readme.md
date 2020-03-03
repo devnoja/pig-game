7f0038df73760df86973c6a0c96171e1085da7bc
=noja <noelnoja8@gmail.com>
2020-03-03 20:56:28 +0300

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,15 @@
+# A simple dice game also known as pig game
+
+## Rules
+
+In each turn player rolls a die repeatedly until either a 1 is rolled or player decides to hold and pass on the turn to its opponent.
+
+The number player rolls is added to the current score. If player rolls a 1, they lose their current score and turn is passed to their opponent.
+
+If player gets any other number, they can decide to continue and roll the die.
+
+If player decides to hold, then their current scores are added to overall score.
+
+The first player to score 100 wins the game.
+
+**NB: The players can decide to set predetermined score at the beginning of the game**
---
+++ app.js
@@ -0,0 +1,98 @@
+let score, roundScore, activePlayer, gamePlaying;
+init();
+
+document.querySelector(".btn-roll").addEventListener("click", function() {
+  if (gamePlaying) {
+    //Generate random number
+    let dice = Math.floor(Math.random() * 6) + 1;
+
+    //Display Results
+    let diceDOM = document.querySelector(".dice");
+    diceDOM.src = `dice-${dice}.png`;
+    diceDOM.style.display = "block";
+
+    //Update the Round score IF the dice !== 1
+    if (dice !== 1) {
+      //Add Score
+      roundScore += dice;
+      document.querySelector(
+        `#current-${activePlayer}`
+      ).textContent = roundScore;
+    } else {
+      //Next player turn
+      nextPlayer();
+    }
+  }
+});
+
+document.querySelector(".btn-hold").addEventListener("click", function() {
+  if (gamePlaying) {
+    // Add current Score to the Global Score
+    score[activePlayer] += roundScore;
+
+    // Update UI
+    document.getElementById(`score-${activePlayer}`).textContent =
+      score[activePlayer];
+
+    // Check if a player won a game
+    let finalScore = document.querySelector(".final-score").value;
+    let winningScore;
+    if (finalScore) {
+      winningScore = finalScore;
+    } else {
+      winningScore = 100;
+    }
+    if (score[activePlayer] >= winningScore) {
+      document.getElementById(`name-${activePlayer}`).textContent = "winner!";
+      document.querySelector(".dice").style.display = "none";
+      document
+        .querySelector(`.player-${activePlayer}-panel`)
+        .classList.add("winner");
+      document
+        .querySelector(`.player-${activePlayer}-panel`)
+        .classList.remove("active");
+      gamePlaying = false;
+    } else {
+      // Next Player
+      nextPlayer();
+    }
+  }
+});
+
+function nextPlayer() {
+  activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+  roundScore = 0;
+
+  document.getElementById("current-0").textContent = "0";
+  document.getElementById("current-1").textContent = "0";
+
+  document.querySelector(".player-0-panel").classList.toggle("active");
+  document.querySelector(".player-1-panel").classList.toggle("active");
+
+  document.querySelector(".dice").style.display = "none";
+}
+
+document.querySelector(".btn-new").addEventListener("click", init);
+
+function init() {
+  score = [0, 0];
+  roundScore = 0;
+  activePlayer = 0;
+  gamePlaying = true;
+
+  document.querySelector(".dice").style.display = "none";
+
+  document.getElementById("score-0").textContent = "0";
+  document.getElementById("score-1").textContent = "0";
+  document.getElementById("current-0").textContent = "0";
+  document.getElementById("current-1").textContent = "0";
+  document.getElementById(`name-0`).textContent = "Player 1";
+  document.getElementById(`name-1`).textContent = "Player 2";
+
+  document.querySelector(`.player-0-panel`).classList.remove("winner");
+  document.querySelector(`.player-1-panel`).classList.remove("winner");
+  document.querySelector(`.player-0-panel`).classList.remove("active");
+  document.querySelector(`.player-1-panel`).classList.remove("active");
+
+  document.querySelector(`.player-0-panel`).classList.add("active");
+}
